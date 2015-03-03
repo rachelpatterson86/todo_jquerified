78530367d8db66eb7b1f783c33c4fa111de7abf0
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-03 00:56:49 -0500

    add user_id fk

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303050440) do
+ActiveRecord::Schema.define(version: 20150303055438) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "list"
     t.boolean  "task_complete"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
