8694a71e5b9a2d766a9977c67b2fc74776d99851
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-03 00:28:52 -0500

    todo and user contr

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,23 @@
+class TodosController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+end
