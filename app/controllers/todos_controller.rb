734a6144b495342d9113401ebc47a2310bd0c765
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-03 01:55:52 -0500

    added more logic, remove routes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,23 +1,31 @@
 class TodosController < ApplicationController
+before_action :set_user_todos, only: [:update, :destroy]
   def index
-  end
-
-  def show
-  end
-
-  def new
+    @todo = current_user.todos.all
   end
 
   def create
+    @todo = current_user.todos.create(todo_params)
   end
 
   def update
+    @todo.update(todo_params)
   end
 
   def destroy
+    @todo.delete
   end
 
   def edit
   end
+
+private
+    def set_user_todos
+      @todo = current_user.todos.find(params[:id])
+    end
+
+    def todo_params
+      params.require(:todo).permit(:list,:task_complete,:user_id)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :user, only: :index do
-    resources :todo
+    resources :todo, except: [:show, :new, :edit]
   end
 root to: "user#index"
   # You can have the root of your site routed with "root"
