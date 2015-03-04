d10225e82a44fd025718bb5d5c60a065112706da
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-04 12:48:51 -0500

    add edit stuff

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-before_action :set_user_todos, only: [:update, :destroy]
+before_action :set_user_todos, only: [:update, :edit, :destroy]
 before_action :set_user
   def index
     @todo = Todo.all
@@ -27,7 +27,16 @@ before_action :set_user
   end
 
   def update
-    @todo.update(todo_params)
+    respond_to do |format|
+      if @todo.update(todo_params)
+        format.html { redirect_to user_todos_path(@todo), notice: 'todo was successfully created.' }
+        format.json { render :show, status: :created, location: @todo }
+      else
+        format.html { render :new }
+        format.json { render json: @todo.errors, status: :unprocessable_entity }
+      end
+    end
+
   end
 
   def destroy
